5c4adc0ac9e95afac4a4dd527617e1d67b293f8b
Wenceslau Neto <wbaneto@gmail.com>
2025-04-29 19:57:12 +0100

    feat: rename packages and update pom.xml for prp10-web-spring-data4 module

--- prp10-web-spring-data4/src/main/java/br/com/ada/springdata4/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ada.springdata4.repositories;
-
-import br.com.ada.springdata4.entities.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-}
