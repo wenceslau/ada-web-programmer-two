e47f69ef7948bf380f45716d679adf099250f8ab
Wenceslau Neto <wbaneto@gmail.com>
2025-05-10 19:09:36 +0100

    feat: add BookController and pom.xml for web-spring-data-simple-sql-init module

---
+++ web-spring-data-simple-sql-init/src/main/java/br/com/ada/springdatasimplesqlinit/controllers/BookController.java
@@ -0,0 +1,77 @@
+package br.com.ada.springdatasimplesqlinit.controllers;
+
+import br.com.ada.springdatasimplesqlinit.entities.Book;
+import br.com.ada.springdatasimplesqlinit.repositories.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping
+    public Page<Book> getBooks(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "title") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        Sort sort = direction.equalsIgnoreCase("desc") ?
+                Sort.by(sortBy).descending() :
+                Sort.by(sortBy).ascending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return bookRepository.findAll(pageable);
+    }
+
+//    @GetMapping
+//    public List<Book> getBooks() {
+//        return bookRepository.findAll();
+//    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        bookRepository.save(book);
+        return book;
+    }
+
+    @PutMapping("/{isbn}")
+    public Book editBook(@PathVariable String isbn, @RequestBody Book book) {
+        Book existingBook = bookRepository.findByIsbn(isbn)
+                .orElse(null);
+
+        if (existingBook == null) {
+            throw new RuntimeException("Book not found");
+        }
+
+        existingBook.setAuthor(book.getAuthor());
+        existingBook.setTitle(book.getTitle());
+
+        bookRepository.save(existingBook);
+
+        return existingBook; // or throw an exception
+    }
+
+    @DeleteMapping("/{isbn}")
+    public void deleteBook(@PathVariable String isbn) {
+        Book existingBook = bookRepository.findByIsbn(isbn)
+                .orElse(null);
+
+        if (existingBook == null) {
+            throw new RuntimeException("Book not found");
+        }
+
+        bookRepository.delete(existingBook);
+    }
+}
+
+
---
+++ web-spring-data-simple-sql-init/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+CREATE TABLE books
+(
+    id     BIGINT       NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    isbn   VARCHAR(13)  NOT NULL,
+    title  VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE books  ADD CONSTRAINT books_isbn_unique UNIQUE (isbn);
+
+CREATE SEQUENCE books_seq START WITH 1 INCREMENT BY 50;
+
+INSERT INTO books (id, author, isbn, title)
+VALUES (nextval('books_seq'), 'J.K. Rowling', '9780747532743', 'Harry Potter and the Philosopher Stone');
+
+INSERT INTO books (id, author, isbn, title)
+VALUES (nextval('books_seq'), 'J.R.R. Tolkien', '9780261103573', 'The Lord of the Rings');
+
+INSERT INTO books (id, author, isbn, title)
+VALUES (nextval('books_seq'), 'George Orwell', '9780451524935', '1984');
+
